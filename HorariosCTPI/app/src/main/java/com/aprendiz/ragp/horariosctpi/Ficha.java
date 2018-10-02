a45cceb74975fb3e8f3d9f8dc6c86d2d43de1e2c
Rodrigo Alejandro Guspian Perez <raguspian@misena.edu.co>
2018-10-02 16:47:19 -0500

    clases terminadas

---
+++ HorariosCTPI/app/src/main/java/com/aprendiz/ragp/horariosctpi/Ficha.java
@@ -0,0 +1,31 @@
+package com.aprendiz.ragp.horariosctpi;
+
+public class Ficha {
+    private String instructor;
+    private String numero;
+    private String programa;
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getPrograma() {
+        return programa;
+    }
+
+    public void setPrograma(String programa) {
+        this.programa = programa;
+    }
+}
---
+++ HorariosCTPI/app/src/main/java/com/aprendiz/ragp/horariosctpi/Programa.java
@@ -0,0 +1,40 @@
+package com.aprendiz.ragp.horariosctpi;
+
+public class Programa {
+    private String descripcion;
+    private String icono;
+    private String nombre;
+    private String video;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getIcono() {
+        return icono;
+    }
+
+    public void setIcono(String icono) {
+        this.icono = icono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+}
