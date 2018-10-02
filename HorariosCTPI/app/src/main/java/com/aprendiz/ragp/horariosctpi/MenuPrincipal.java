0291205bae75bf76990c938826ea9b2b5fec012e
DavidPyn27 <popayan159@gmail.com>
2018-10-02 16:45:09 -0500

    uno

--- HorariosCTPI/app/src/main/java/com/aprendiz/ragp/horariosctpi/MenuPrincipal.java
+++ HorariosCTPI/app/src/main/java/com/aprendiz/ragp/horariosctpi/MenuPrincipal.java
@@ -2,11 +2,13 @@ package com.aprendiz.ragp.horariosctpi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MenuPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
     }
