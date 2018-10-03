30ca00542d3f1e2c3a0df8e9a6d40905fc43d8e3
DavidPyn27 <popayan159@gmail.com>
2018-10-03 11:34:15 -0500

    ca

---
+++ HorariosCTPI/app/src/main/java/com/aprendiz/ragp/horariosctpi/controllers/HorarioManana.java
@@ -0,0 +1,15 @@
+package com.aprendiz.ragp.horariosctpi.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aprendiz.ragp.horariosctpi.R;
+
+public class HorarioManana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario_manana);
+    }
+}
